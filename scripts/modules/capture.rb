206b34491fda468fb7764fd53ed4cf5be3062c05
Greg Leuch <contact@gleu.ch>
2013-10-02 09:09:18 -0400

    no need to double-up on error output (sidekiq already does)

--- scripts/modules/capture.rb
+++ scripts/modules/capture.rb
@@ -314,7 +314,7 @@ module IsTheInternet
 
         rescue => err
           ActiveRecord::Base.connection.reconnect! if err.to_s.match(/mysql/i)
-          _error(err, 1)
+          # _error(err,x 1)
           error!(err)
 
         ensure
